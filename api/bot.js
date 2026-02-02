9151c56bd99c56eebbe42de21f40491078d7df46
Danxy Official ☕︎ <dpras19800@gmail.com>
2026-02-03 05:51:43 +0700

    Update webhook URL to new deployment domain

--- api/bot.js
+++ api/bot.js
@@ -29,7 +29,7 @@ app.post('/api/bot', async (req, res) => {
 
 app.get('/set-webhook', async (req, res) => {
   try {
-    const vercelUrl = 'https://script-bot-teleram-vercel.vercel.app';
+    const vercelUrl = 'https://GANTI-LINK-NYA-SESUI-DOMAIN-HASIL-DEPLOY.vercel.app';
     const webhookUrl = `${vercelUrl}/api/bot`;
     
     await bot.deleteWebHook();
